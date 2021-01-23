534c40c43774e44ca1e923a45eac6b6614075055
Carl de Guia <carl.2795@gmail.com>
2021-01-23 14:23:49 +0800

    setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+// Boot express
+const app: Application = express();
+const port = 5000;
+
+// Application routing
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).send({ data: 'Hello from the other side' });
+});
+
+// Start server
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
