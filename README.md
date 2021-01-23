123a299d0f705baf05051a4f5c37e9c475389523
Carl de Guia <carl.2795@gmail.com>
2021-01-23 14:38:03 +0800

    setup mongoose

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # budget-tracker-api
+
+## Setup
+
+1. Install libraries using `npm install`
+2. To build, run `npm run build`
+3. When coding, run `npm run start`
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+/* eslint-disable no-console */
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 // Boot express
 const app: Application = express();
 const port = 5000;
+
+mongoose.connect('mongodb://localhost/tropogs', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to db!');
+});
 
 // Application routing
 app.use('/', (req: Request, res: Response) => {
@@ -10,5 +19,4 @@ app.use('/', (req: Request, res: Response) => {
 });
 
 // Start server
-// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
